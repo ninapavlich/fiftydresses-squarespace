1c0823e5dd99d36421c90704533309ae5692c0c5
Nina Pavlich <nina@ninalp.com>
2015-09-06 00:03:35 -0400

    working on homepage

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,3 +1,6 @@
+var medium_breakpoint = 700;
+var small_breakpoint = 500;
+
 $( document ).ready(function() {
     console.log( "ready!" );
     resizeGridItems();
@@ -13,7 +16,15 @@ $( document ).ready(function() {
 
 /* GRID STUFF */
 function resizeGridItems(){
-    var grid_item_width = $('.grid').width() / 3;
+    var grid_width = $('.grid').width();
+
+    if(grid_width > medium_breakpoint){
+        var grid_item_width = grid_width / 3;
+    }else if(grid_width <= medium_breakpoint && grid_width > small_breakpoint){
+        var grid_item_width = grid_width / 2;
+    }else{
+        var grid_item_width = grid_width;
+    }
 
     $(".grid-item").css("width", grid_item_width);
     $(".grid-item").css("height", grid_item_width);
@@ -22,4 +33,5 @@ function resizeGridItems(){
     $(".inner-grid-item").css("height", grid_item_width);
 
     $(".grid-item.double").css("height", 2*grid_item_width);
+    
 }
