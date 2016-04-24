c0b4bec94ab3b69f13e3e1a3d7a7535b40dc18aa
Nina Pavlich <nina@ninalp.com>
2016-04-24 19:11:00 -0400

    working on iamgefill

--- scripts/custom.js
+++ scripts/custom.js
@@ -105,7 +105,7 @@ $( document ).ready(function() {
 
     gallery_items = getGalleryItems();
 
-    $(document).fitImage();  
+    $(document).fitImage({attributeSelector:".fill"});  
     
 
     /* Listeners */
